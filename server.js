31297470c8acbcef9589b66886d79a02ca75292b
FelipeHidekiTanji <felipe.tanji@escola.pr.gov.br>
2026-03-13 11:01:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const conecxao = mysql.createConnection({
+const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     datbase: 'escola'
+});
+
+app.post("/salvar", (req, res) => {
+    let nome = req.body.nome;
+    let idade = req.body.idade;
+
+    let sql = "INSERT INTO alunos (nome, idade) VALUES (?, ?);"
+
+    conexao.query(sql, [nome, idade], (erro, resultado) => {
+        if (erro) {
+            console,log(erro);
+        }   else {
+            res.send("Aluno salvo com sucesso!");
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
 });
